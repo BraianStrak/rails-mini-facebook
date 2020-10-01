4fa31300f2f9523e7a3f566de20eb05db6c27d5d
BraianStrak <braianstrak1999@gmail.com>
2020-10-01 22:38:51 +0100

    New root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
