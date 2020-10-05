f46e7611705073bfae92492728119091a609f070
BraianStrak <braianstrak1999@gmail.com>
2020-10-05 22:49:12 +0100

    Basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'users#show', as: :authenticated_root
+      root 'posts#index', as: :authenticated_root
     end
   
     unauthenticated do
