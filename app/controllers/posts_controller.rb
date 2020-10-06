2d38c905d74922e0d863dca88cdedc981c10ab3a
BraianStrak <braianstrak1999@gmail.com>
2020-10-06 21:56:58 +0100

    Posting form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
     def index
-        
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save 
+            redirect_to authenticated_root
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:user_id, :title, :body)
     end
 end
