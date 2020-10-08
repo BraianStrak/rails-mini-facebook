c866547156e22440c669eb270b8c079a5aff6cfa
BraianStrak <braianstrak1999@gmail.com>
2020-10-08 22:02:11 +0100

    Like model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,25 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
+        @post = current_user.posts.build
     end
 
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
 
     def create
-        @post = Post.new(post_params)
-
-        if @post.save 
-            redirect_to authenticated_root
-        end
+        @post = current_user.posts.build(post_params)
+        @post.save
+        redirect_to root_path
     end
 
     private
 
     def post_params
-        params.require(:post).permit(:user_id, :title, :body)
+        params.require(:post).permit(:title, :body)
+        #I dont think the error is here as commenting this out causes the same error 
+        # do i need user id here and in the form as hidden field?
+        # <%= post_form.hidden_field :user_id, value: current_user.id %><br>
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def show
         #profile page 
         @user = current_user
+        @posts = current_user.posts
     end
 
     def index
