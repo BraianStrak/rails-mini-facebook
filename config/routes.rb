42b7eb8dfefbc4a22bc3c5f64e7cf9ea068c7d65
BraianStrak <braianstrak1999@gmail.com>
2020-10-08 22:23:30 +0100

    Implemented liking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   resources :posts
-  resources :likes
+  resources :posts do
+    resources :likes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_scope :user do
