b5b7b5c1195e545816b399da5936c77bdd17b3ba
BraianStrak <braianstrak1999@gmail.com>
2020-10-10 17:39:23 +0100

    Completed comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
-end
+    def create
+        @comment = current_user.comments.build(comment_params)
+        @comment.save
+        redirect_to posts_path
+    end
+    
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body, :post_id)
+    end
+end 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
         @post = current_user.posts.build
+        @comment = current_user.comments.build
     end
 
     def new
@@ -11,15 +12,12 @@ class PostsController < ApplicationController
     def create
         @post = current_user.posts.build(post_params)
         @post.save
-        redirect_to root_path
+        redirect_to posts_path
     end
 
     private
 
     def post_params
         params.require(:post).permit(:title, :body)
-        #I dont think the error is here as commenting this out causes the same error 
-        # do i need user id here and in the form as hidden field?
-        # <%= post_form.hidden_field :user_id, value: current_user.id %><br>
     end
 end
